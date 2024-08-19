098933437811f38d46defd4317d67a58e6f980ed
Samira Arabgol <samiraarabgol@Samiras-MacBook-Pro.local>
2024-08-18 17:58:30 -0700

    add table

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Alert from '@mui/material/Alert';
 import GoogleBarChart from './BarChart';
+import GoogleTable from './Table';
 import './App.css';
 
 const useFetchPlayers = (url) => {
@@ -34,13 +36,21 @@ function App() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={1} rowSpacing={2} columnSpacing={2}>
+      <Grid spacing={3} item xs={12} sx={{ margin: 6 }}>
+        <Alert variant="outlined" severity="info">
+        This web app is currently under development, and all rights are reserved by Techeeta Group.
+        </Alert>
+        </Grid>
+        <Grid item xs={11.5} sx={{ margin: 2 }}>
+          <GoogleTable rows={players} />
+        </Grid>
         <Grid item xs={12}>
           <GoogleBarChart id="chart1" data={players} options={{
             yAxis: 'avg_speed',
             yLabel: "Average Speed",
             title: "Players Average Speed",
-            colors: ['#b87333']
+            colors: ['#76A7FA']
           }} />
         </Grid>
         <Grid item xs={12}>
@@ -48,7 +58,7 @@ function App() {
             yAxis: 'distance',
             yLabel: "Distance",
             title: "Players Distance",
-            colors: ['#1b9e72']
+            colors: ['#C5A5CF']
           }} />
         </Grid>
       </Grid>
--- client/src/Table.js
+++ client/src/Table.js
@@ -1,54 +1,192 @@
-import React, { useEffect } from 'react';
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
-const GoogleTable = () => {
-  useEffect(() => {
-    // Function to load Google Charts and draw the table
-    const loadGoogleCharts = () => {
-      // Load the Google Charts library
-      const script = document.createElement('script');
-      script.src = 'https://www.gstatic.com/charts/loader.js';
-      script.onload = () => {
-        // Load the necessary packages
-        window.google.charts.load('current', { packages: ['table'] });
-        window.google.charts.setOnLoadCallback(drawTable);
-      };
-      document.body.appendChild(script);
-    };
 
-    // Function to draw the table
-    const drawTable = () => {
-      const data = new window.google.visualization.DataTable();
-      data.addColumn('string', 'Name');
-      data.addColumn('number', 'Salary');
-      data.addColumn('boolean', 'Full Time Employee');
-      data.addRows([
-        ['Mike', { v: 10000, f: '$10,000' }, true],
-        ['Jim', { v: 8000, f: '$8,000' }, false],
-        ['Alice', { v: 12500, f: '$12,500' }, true],
-        ['Bob', { v: 7000, f: '$7,000' }, true]
-      ]);
+const columns = [
+  { id: 'player_name', label: 'Name', minWidth: 220 },
+  { id: 'number_of_sprints', label: 'Num of Sprints', minWidth: 110,  align: 'center' },
+  {
+    id: 'minutes_played',
+    label: 'Min Played',
+    minWidth: 100,
+    align: 'center'
+  },
+  {
+    id: 'avg_speed',
+    label: 'Avg. Speed (km/h)',
+    minWidth: 130,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'max_speed',
+    label: 'Max Speed (km/h)',
+    minWidth: 130,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'distance',
+    label: 'Distance(km)',
+    minWidth: 100,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'HID_distance',
+    label: 'HID Distance (>15km/h)',
+    minWidth: 170,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'distance_speed_range_below_15',
+    label: '[0-15]',
+    minWidth: 120,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'distance_speed_range_20-to_25',
+    label: '[20-25]',
+    minWidth: 120,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'distance_speed_range_25_to_30',
+    label: '[25-30]',
+    minWidth: 120,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'distance_speed_range_over_30',
+    label: 'Over 30',
+    minWidth: 100,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'max_acceleration',
+    label: 'Max',
+    minWidth: 100,
+    align: 'center',
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: 'number_of_accelerations_over_3',
+    label: 'Over 3',
+    minWidth: 100,
+    align: 'center'
+  },
+  {
+    id: 'number_of_accelerations_over_4',
+    label: 'Over 4',
+    minWidth: 100,
+    align: 'center'
+  },
+  {
+    id: 'number_of_descelerations_over_3',
+    label: 'Over 3',
+    minWidth: 100,
+    align: 'center'
+  },
+  {
+    id: 'number_of_descelertions_over_4',
+    label: 'Over 4',
+    minWidth: 100,
+    align: 'center'
+  }
+];
 
-      const table = new window.google.visualization.Table(document.getElementById('table_div'));
+export default function StickyHeadTable({rows}) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-      table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
-    };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    loadGoogleCharts(); // Load Google Charts when the component mounts
-
-    // Clean up function to remove script from document
-    return () => {
-      const script = document.querySelector('script[src="https://www.gstatic.com/charts/loader.js"]');
-      if (script) {
-        document.body.removeChild(script);
-      }
-    };
-  }, []);
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
-    <div>
-      <div id="table_div" style={{ width: '100%', height: '400px' }}></div>
-    </div>
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+          <TableRow>
+              <TableCell sx={{ fontWeight: 'bold', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.5)' }} size={'small'} align="center" colSpan={7}>
+                --------
+              </TableCell>
+              <TableCell sx={{ fontWeight: 'bold', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.5)' }} size={'small'} align="center" colSpan={4}>
+              Distance Speed Range (km/h)
+              </TableCell>
+              <TableCell sx={{ fontWeight: 'bold', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.5)' }} size={'small'} align="center" colSpan={3}>
+              Acceleration (m/s²)
+              </TableCell>
+              <TableCell sx={{ fontWeight: 'bold', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.5)' }} size={'small'} align="center" colSpan={2}>
+              Decelerations (m/s²)
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                sx={{ fontWeight: 'bold'}}
+                size={'small'}
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                return (
+                  <TableRow  sx={{
+                    backgroundColor: index % 2 === 0 ? '#f5f5f5' : 'white'
+                  }}
+                  hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell size={'small'} key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
-};
-
-export default GoogleTable;
+}
